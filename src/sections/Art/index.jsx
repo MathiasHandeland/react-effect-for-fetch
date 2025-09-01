7dbd00f10b6af35feb8754cf0e51f6277347a87b
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-01 09:49:48 +0200

    arts section complete

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,45 @@
+import { useEffect, useState } from "react";
+
 function ArtsSection() {
+
+  const baseUrl = "https://boolean-uk-api-server.fly.dev";
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await  fetch(`${baseUrl}/art`);
+      const jsonData = await response.json();
+      setData(jsonData);
+    }
+    fetchData();
+  }, []);
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+        <ul className="art-list">
+          <li>
+            
+            {data.map((artwork) => (
+              <div key={artwork.id} className="artwork">
+                <div className="frame">
+                  <img
+                    src={`${baseUrl}${artwork.imageURL}`}
+                    alt={artwork.title}
+                  />
+                </div>
+                <h3>{artwork.title}</h3>
+                <p>Artist: {artwork.artist}</p>
+                <h4>Publication History:</h4>
+                <ul>
+                  {artwork.publicationHistory.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </li>
+        </ul>
     </section>
   )
 }
